0f3f839f3890a51faa9c4c8e4db28db384a911df
Joel Freyss <jfreyss@users.noreply.github.com>
2016-08-11 00:17:30 +0200

    new page layout / first text

---
+++ js/main.js
@@ -0,0 +1,9 @@
+<!-- Start of StatCounter Code for Default Guide -->
+var sc_project=11069810; 
+var sc_invisible=1; 
+var sc_security="18a1847b"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
